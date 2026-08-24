6b3cbd41d438b9b4970aa4ad949bd83e1eb8aa6c
thecanhhuynh <thecanhhuynh@gmail.com>
2026-08-24 09:04:04 +0700

    init: Setup project, Docker and config

---
+++ src/main/java/com/htc/RealtimeAuctionSystem/RealtimeAuctionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.htc.RealtimeAuctionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeAuctionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeAuctionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/htc/RealtimeAuctionSystem/RealtimeAuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htc.RealtimeAuctionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeAuctionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
